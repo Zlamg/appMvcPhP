7b9c51acabf5346be163faa0841c9cd203846d8c
Mauricio <mauricio.ar.ga@gmail.com>
2018-05-10 09:03:32 -0500

    commit 3

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('appmvcphp-mysqldbserver.mysql.database.azure.com;dbname=mysampledb;charset=utf8', 'zlamg@appmvcphp-mysqldbserver', 'Mauriy11');
+        $pdo = new PDO('mysql:host=appmvcphp-mysqldbserver.mysql.database.azure.com;dbname=mysampledb;charset=utf8', 'zlamg@appmvcphp-mysqldbserver', 'Mauriy11');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
